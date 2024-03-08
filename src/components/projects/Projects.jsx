9c6205612dd2ed7fbc1a1d11540867d7d36e72ab
Eliane Fortes <elimrfortes@gmail.com>
2024-03-08 18:17:37 +1100

    projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,59 +6,66 @@ import IMGCSS from '../../assets/project-images/csscourse.png';
 import IMGBOAT from '../../assets/project-images/boat.png';
 import Filter01 from '../../assets/project-images/filter.png';
 
+const data = [
+  {
+    id: 1,
+    titleh3: 'UHost(fictional business)',
+    titleh4: 'A website that demonstrate the use modern CSS features, such as Flexbox and CSS grid.',
+    image: IMGCSS,
+    github: 'https://elifortes.github.io/CSS-course/',
+    demo: 'https://github.com/elifortes/CSS-course.git',
+  },
+  {
+    id: 2,
+    titleh3: 'Airbnb Boat(fictional business)',
+    titleh4: 'Is a conceptual project that illustrates a modern platform for renting boats.',
+    image: IMGBOAT,
+    github: 'https://github.com/elifortes/Airbnb-Boat.git',
+    demo: 'https://github.com/elifortes/Airbnb-Boat.git',
+  },
+  {
+    id: 3,
+    titleh3: 'Gift Genius',
+    titleh4: 'Its a app and blabla.',
+    image: IMGBOAT,
+    github: 'https://github.com/elifortes/Airbnb-Boat.git',
+    demo: 'https://github.com/elifortes/Airbnb-Boat.git',
+  },
+  {
+    id: 4,
+    titleh3: 'Instagram Filters',
+    titleh4: 'Showcasing engaging and interactive Instagram filters created with Spark AR Studio.',
+    image: Filter01,
+    github: 'https://www.instagram.com/ar/1616459879160433/',
+    demo: 'https://www.instagram.com/ar/1616459879160433/',
+  },
+]
 
 const Projects = () => {
   return (
     <section id='projects'>
       <h2>Software Development</h2>
       <div className='container portfolio_container'>
-        <article className='portfolio_item'>
-          <h3>UHost(fictional business)</h3>
-          <h4>A website that demonstrate the use modern CSS features, such as Flexbox and CSS grid.</h4>
-
+      {
+        data.map(({id, titleh3, titleh4, image, github, demo}) => {
+          return (
+          <article key={id} className='portfolio_item'>
+          <h3>{titleh3}</h3>
+          <h4>{titleh4}</h4>
             <div className='portfolio_item-image'>
-              <a href='https://elifortes.github.io/CSS-course/'>
-              <img src={IMGCSS} alt='' style={{width:'250px', height:'auto'}} />
+              <a href={github}>
+              <img src={image} alt='' style={{width:'250px', height:'auto'}} />
               </a>
               <div className='portfolio_item-cta'>
-                <a href='https://github.com/elifortes/CSS-course.git' target='_blank'><FaGithub style={{color:'black'}}/></a>
+                <a href={demo} target='_blank'><FaGithub style={{color:'black'}}/></a>
                 <h5>JS CSS Flexbox Grid</h5>
               </div>
             </div>
         </article>
 
-
-        <article className='portfolio_item'>
-          <h3>Airbnb Boat(fictional business)</h3>
-          <h4>Is a conceptual project that illustrates a modern platform for renting boats.</h4>
-
-            <div className='portfolio_item-image'>
-            <a href='https://github.com/elifortes/Airbnb-Boat.git'>
-            <img src={IMGBOAT} alt='' style={{width:'250px', height:'auto'}} />
-            </a>
-              <div className='portfolio_item-cta'>
-                <a href='https://github.com/elifortes/Airbnb-Boat.git' target='_blank'><FaGithub style={{color:'black'}}/></a>
-                <h5>Ruby-on-Rails JS Bootstrap Heroku</h5>
-              </div>
-            </div>
-        </article>
-      </div>
-
-      <h2>Digital Creativity</h2>
-      <div className='portfolio_container-filter'>
-        <article className='portfolio_item-filter'>
-            <h3>Instagram Filters</h3>
-            <h4>Showcasing engaging and interactive Instagram filters created with Spark AR Studio.</h4>
-          <div className='portfolio_item-image-filter'>
-              <a href='https://www.instagram.com/ar/1616459879160433/'>
-              <img src={Filter01} alt='' style={{width:'250px', height:'auto'}} />
-              </a>
-            <div className='portfolio_item-cta'>
-                <a href='https://www.instagram.com/ar/1616459879160433/' target='_blank'><FaInstagram style={{color:'black'}}/></a>
-                <h5>Canva Spark-AR-Studio Photoshop JS </h5>
-            </div>
-          </div>
-        </article>
+          )
+        })
+      }
       </div>
     </section>
   );
